0eabad659d0d1f9d9f75c107ecfc2ea27a27e6b2
tomknk <tomita.kanako.feb@gmail.com>
2023-11-19 23:15:42 +0900

    getterメソッド削除

--- src/baseball/Review03.java
+++ src/baseball/Review03.java
@@ -6,7 +6,6 @@ public class Review03 {
 
        BaseBallTeam bbt01= new BaseBallTeam();
        bbt01.setName("東京ヤクルトスワローズ");
-       String bbt01Name = bbt01.getName();
        bbt01.setWin(80);
        int bbt01Win = bbt01.getWin();
        bbt01.setLose(59);
@@ -17,7 +16,6 @@ public class Review03 {
 
        BaseBallTeam bbt02= new BaseBallTeam();
        bbt02.setName("横浜DeNAベイスターズ");
-       String bbt02Name = bbt02.getName();
        bbt02.setWin(73);
        int bbt02Win = bbt02.getWin();
        bbt02.setLose(68);
@@ -28,7 +26,6 @@ public class Review03 {
 
        BaseBallTeam bbt03= new BaseBallTeam();
        bbt03.setName("阪神タイガース");
-       String bbt03Name = bbt03.getName();
        bbt03.setWin(68);
        int bbt03Win = bbt03.getWin();
        bbt03.setLose(71);
@@ -39,7 +36,6 @@ public class Review03 {
 
        BaseBallTeam bbt04= new BaseBallTeam();
        bbt04.setName("読売ジャイアンツ");
-       String bbt04Name = bbt03.getName();
        bbt04.setWin(68);
        int bbt04Win = bbt04.getWin();
        bbt04.setLose(72);
@@ -50,7 +46,6 @@ public class Review03 {
 
        BaseBallTeam bbt05= new BaseBallTeam();
        bbt05.setName("広島東洋カープ");
-       String bbt05Name = bbt03.getName();
        bbt05.setWin(66);
        int bbt05Win = bbt05.getWin();
        bbt05.setLose(74);
@@ -61,7 +56,6 @@ public class Review03 {
 
        BaseBallTeam bbt06= new BaseBallTeam();
        bbt06.setName("中日ドラゴンズ");
-       String bbt06Name = bbt06.getName();
        bbt06.setWin(66);
        int bbt06Win = bbt06.getWin();
        bbt06.setLose(75);
